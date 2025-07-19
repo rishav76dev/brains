5d26a55711cea67e485da5e22e37945a260baefc
rishav76dev <rjrishav037@gmail.com>
2025-07-19 10:48:23 +0530

    twittter link working

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,12 @@
+// vite.config.js or vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     headers: {
-      'Content-Security-Policy': "frame-src 'self' https://www.youtube.com https://youtube.com;"
+      'Content-Security-Policy': "frame-src 'self' https://www.youtube.com https://youtube.com https://platform.twitter.com;"
     }
   }
 })
