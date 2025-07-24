5fa8405a5372e12060c36c05ef5ceb993de79254
rishav76dev <rjrishav037@gmail.com>
2025-07-24 14:00:08 +0530

    logout button fix

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { AllIcon } from "../icons/AllIcon";
 import DocumentIcon from "../icons/DocumentIcon";
 import { Logo } from "../icons/Logo";
@@ -13,17 +14,17 @@ export function Sidebar({
 }: {
   onSelectType: (type: string | null) => void;
 }) {
+  const navigate = useNavigate();
   return (
     <div
-  className="
+      className="
     hidden sm:fixed sm:top-0 sm:left-0 sm:h-screen
     sm:border-r sm:pl-6 sm:pr-4 sm:py-6
     sm:flex sm:flex-col sm:w-56 lg:w-64
     bg-gradient-to-b from-purple-50 via-purple-100 to-purple-200
      border-r-8 border-purple-300
   "
->
-
+    >
       <div className="flex items-center space-x-2 text-purple-600 mb-8">
         <Logo />
         <span className="text-xl font-semibold text-gray-800">Brainly</span>
@@ -57,11 +58,7 @@ export function Sidebar({
         {/* Bottom logout button */}
         <div className="pt-8 w-45 h-20">
           <Button
-            onClick={() =>
-              logout((path: string) => {
-                window.location.href = path;
-              })
-            }
+            onClick={() => logout(navigate)}
             variant="primary"
             text="Logout"
             startIcon={<Logout />}
