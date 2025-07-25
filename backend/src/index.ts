dd6fd19607d7e36bdd67d5c9d47339f984d2db90
rishav76dev <rjrishav037@gmail.com>
2025-07-25 18:55:45 +0530

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,24 @@
 import express from "express";
+import job from "./utils/cron";
 import dotenv from "dotenv";
 import connectDB from "./utils/connectDB";
 import cors from "cors"
 import AuthRoute from './routes/authRoutes'
 import UserRoute from './routes/userRoutes'
 
+dotenv.config();
 //initialize express app
 const app = express();
 
 connectDB()
+
+job.start();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
 
-
-dotenv.config();
 
 app.use("/api/v1/user",AuthRoute )
 app.use("/api/v1",UserRoute)
