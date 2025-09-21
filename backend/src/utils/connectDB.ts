5efef3175189213ba7a600929330c27e5df17ee8
rishav76dev <rjrishav037@gmail.com>
2025-09-22 00:45:34 +0530

    feat: docker setup for local developmen

--- backend/src/utils/connectDB.ts
+++ backend/src/utils/connectDB.ts
@@ -4,6 +4,7 @@ async function connectDB(): Promise<void> {
   try {
     await mongoose.connect(process.env.MONGO_URI as string);
     console.log("✅ MongoDB Connected Successfully");
+    console.log(process.env.MONGO_URI)
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error);
     process.exit(1);
